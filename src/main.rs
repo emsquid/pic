14b42acd3087fbf9ce09a1752631f5bd869def4b
Maratripa <maratripa@gmail.com>
2023-01-30 23:52:17 -0300

    Use wild crate to get wildcard support

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod utils;
 
 fn main() {
     let mut stdout = std::io::stdout();
-    let options = options::Options::parse();
+    let options = options::Options::parse_from(wild::args());
 
     if let Err(err) = previewer::preview(&mut stdout, &options) {
         eprintln!("{err}");
